4e72b24f3ffcbad6e70bc1116f3ec28abaeae15c
Linkzenic <shanecus@gmail.com>
2026-06-21 17:13:00 +0530

    Add Main_Init diagnostics

--- patches/required_patches.c
+++ patches/required_patches.c
@@ -19,35 +19,57 @@ RECOMP_PATCH void Main_Init(void) {
     size_t prevSize;
 
     // @recomp Register base actor extensions.
+    recomp_printf("[MainInitDiag] begin\n");
+    recomp_printf("[MainInitDiag] register_base_actor_extensions begin\n");
     register_base_actor_extensions();
+    recomp_printf("[MainInitDiag] register_base_actor_extensions end\n");
 
     // @recomp_event recomp_on_init(): Allow mods to initialize themselves once.
+    recomp_printf("[MainInitDiag] recomp_on_init begin\n");
     recomp_on_init();
+    recomp_printf("[MainInitDiag] recomp_on_init end\n");
 
+    recomp_printf("[MainInitDiag] osCreateMesgQueue begin\n");
     osCreateMesgQueue(&mq, msg, ARRAY_COUNT(msg));
+    recomp_printf("[MainInitDiag] osCreateMesgQueue end\n");
 
     prevSize = gDmaMgrDmaBuffSize;
     gDmaMgrDmaBuffSize = 0;
 
     // @recomp Load the code segment in the recomp runtime.
+    recomp_printf("[MainInitDiag] recomp_load_overlays begin\n");
     recomp_load_overlays(SEGMENT_ROM_START(code), SEGMENT_START(code), SEGMENT_ROM_END(code) - SEGMENT_ROM_START(code));
+    recomp_printf("[MainInitDiag] recomp_load_overlays end\n");
 
+    recomp_printf("[MainInitDiag] DmaMgr_SendRequestImpl begin\n");
     DmaMgr_SendRequestImpl(&dmaReq, SEGMENT_START(code), SEGMENT_ROM_START(code),
                            SEGMENT_ROM_END(code) - SEGMENT_ROM_START(code), 0, &mq, NULL);
+    recomp_printf("[MainInitDiag] DmaMgr_SendRequestImpl end\n");
+    recomp_printf("[MainInitDiag] Main_InitScreen begin\n");
     Main_InitScreen();
+    recomp_printf("[MainInitDiag] Main_InitScreen end\n");
+    recomp_printf("[MainInitDiag] Main_InitMemory begin\n");
     Main_InitMemory();
+    recomp_printf("[MainInitDiag] Main_InitMemory end\n");
+    recomp_printf("[MainInitDiag] osRecvMesg begin\n");
     osRecvMesg(&mq, NULL, OS_MESG_BLOCK);
+    recomp_printf("[MainInitDiag] osRecvMesg end\n");
 
     gDmaMgrDmaBuffSize = prevSize;
 
+    recomp_printf("[MainInitDiag] Main_ClearMemory begin\n");
     Main_ClearMemory(SEGMENT_BSS_START(code), SEGMENT_BSS_END(code));
+    recomp_printf("[MainInitDiag] Main_ClearMemory end\n");
     
     // @recomp Patch a float that's used to render the clock into the correct value.
     // This is done this way instead of patching the function to avoid conflicts with mods that need to patch the function.
     // The original code is `Matrix_RotateZF(-(timeInSeconds * 0.0175f) / 10.0f, MTXMODE_APPLY);`, where 0.0175f is being used
     // to convert degrees to radians. However, the correct value is PI/180 which is approximately 0.0174533f, and the difference is enough
     // to cause the clock to overshoot when reaching an hour mark.
+    recomp_printf("[MainInitDiag] clock float patch begin\n");
     *(f32*)0x801DDBBC = ((f32)M_PI) / 180.0f;
+    recomp_printf("[MainInitDiag] clock float patch end\n");
+    recomp_printf("[MainInitDiag] end\n");
 }
 
 void Overlay_Relocate(void* allocatedRamAddr, OverlayRelocationSection* ovlRelocs, uintptr_t vramStart);
